7fb1a8992720a4bcf75928d9a1edea429de69a69
Avi Jadhav <avi9421@gmail.com>
2024-04-13 20:27:32 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_project/pages/login_page.dart';
+import 'package:ecommerce_project/utils/routes.dart';
+import 'package:flutter/material.dart';
+import 'pages/cart_page.dart';
+import 'pages/home_page.dart';
+import 'widgets/themes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.cartRoute: (context) => CartPage(),
+      },
+    );
+  }
+}
